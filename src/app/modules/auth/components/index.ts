81b43fedad8e0777213ac95900f502383466dbff
Manish Dube <manishdubeit@gmail.com>
2020-03-04 11:04:50 +0530

    Revert "initial commit"
    This reverts commit 19a836cba548b9fe72f22e372288b5b5acbf57eb.

--- src/app/modules/auth/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { LoginComponent } from "./login/login.component";
-export { NotFoundComponent } from "./404/not-found.component";
-export { SetPasswordComponent } from "./set-password/set-password.component";
